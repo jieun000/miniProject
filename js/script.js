6c61072bb0b0dc3827ec4da67747910fd67f522e
박지은 <mondieu0@naver.com>
2023-01-04 17:23:01 +0900

    4

--- js/script.js
+++ js/script.js
@@ -16,6 +16,19 @@ document.querySelector('.btn3').addEventListener('click', function() {
   document.querySelector('.carousel').style.transform = 'translateX(-200vw)';
   no = 3;
 });
+let no = 1;
+document.querySelector('.previous').addEventListener('click', function() {
+    if(no>1) {
+      document.querySelector('.carousel').style.transform = 'translateX(-'+(no-2)+'00vw)';
+        no--;
+    }
+});
+document.querySelector('.next').addEventListener('click', function() {
+    if(no<3) {
+     document.querySelector('.carousel').style.transform = 'translateX(-' +no+ '00vw)';
+        no++;
+    }
+});
 
 // 다크모드
 let cnt = 0;
@@ -23,11 +36,18 @@ let mode = document.querySelector('#dark');
 document.querySelector('#dark').addEventListener('click', function() {
 cnt++;
 if(cnt % 2 ===1) {
-document.querySelector('#dark').setAttribute('value', '라이트모드'); 
-  document.body.style.background = 'black';
-  document.body.style.color = 'white';   
+document.querySelector('#dark').innerHTML='<i class="fa-solid fa-sun"></i> 라이트모드';
+document.querySelector('#dark').addEventListener('mouseover', function() {
+  document.querySelector('.fa-sun').style.color = 'rgb(255, 155, 6)';
+});
+document.body.style.background = 'black';
+document.body.style.color = 'white';
 } else {
-  document.querySelector('#dark').value='다크모드'; 
+  document.querySelector('#dark').innerHTML='<i class="fa-solid fa-moon"></i> 다크모드';
+  document.querySelector('#dark').title = '시력 보호';
+  document.querySelector('#dark').addEventListener('mouseover', function() {
+    document.querySelector('.fa-moon').style.color = 'yellow)';
+    });
   document.body.style.background = 'white';
   document.body.style.color = 'black';
 }
